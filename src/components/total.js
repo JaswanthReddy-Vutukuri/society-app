c4cca5337d61b9e3e79f8e7a6f163f89cd80c657
JaswanthReddy-Vutukuri <vutukurijaswanth515@gmail.com>
2019-06-29 22:59:04 +0530

    updates data-table

--- src/components/total.js
+++ src/components/total.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Button, Icon, Input, Form } from 'antd';
 import DataTable from './table';
+const ButtonGroup = Button.Group;
 
 const TotalReqs = () => {
   return (
     <div>
       <h2>Total Requests</h2>
+      <div style={{textAlign:'right',margin:'10px 0px'}}>
+      <Form layout="inline">
+        <Form.Item label="Search">
+          {(<Input />)}
+        </Form.Item>
+        <ButtonGroup>
+        <Button>PDF</Button>
+        <Button>CSV</Button>
+        <Button>EXCEL</Button>
+      </ButtonGroup>
+      </Form>
+      </div>
       <DataTable />
     </div>
   );

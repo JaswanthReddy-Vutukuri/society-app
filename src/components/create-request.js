3be5c93e15e49ac28669a28ec715a1d75cddb05a
JaswanthReddy-Vutukuri <vutukurijaswanth515@gmail.com>
2019-07-21 20:37:18 +0530

    updated logic to show request success/failure

--- src/components/create-request.js
+++ src/components/create-request.js
@@ -15,7 +15,8 @@ import {
   Divider,
   Checkbox,
   Button,
-  message
+  message,
+  Modal
 } from 'antd';
 
 const { TextArea } = Input;
@@ -24,25 +25,52 @@ const { Option } = Select;
 class CreateRequest extends React.Component {
 
   state = {
-    confirmDirty: false
+    confirmDirty: false,
+    visible: false
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      visible: false
+    });
+    window.location.reload();
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
   };
 
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        values.MobileNumber = '+'+values.prefix+values.MobileNumber;
+        values.MobileNumber = '+' + values.prefix + values.MobileNumber;
         values.Status = 'New';
         requestService.createRequest(values)
-        .then(
-          ticketNumber => {
-            console.log("ticketNumber:",ticketNumber)
-            message.success(`Request raised Created Successfully. Your Ticket Number: ${ticketNumber}`);
-          },
-          error => {
-            console.log("ERROR:",error)
-          }
-        );
+          .then(
+            ticketNumber => {
+              // message.success(`Request raised Created Successfully. Your Ticket Number: ${ticketNumber}`, 10);
+              this.setState({
+                ModalText: `Request raised successfully. Your Ticket Number: ${ticketNumber}`
+              });
+              this.showModal();
+            },
+            error => {
+              // message.warning('Sorry! Failed to raise the request. Try Again.')
+              this.setState({
+                ModalText: 'Sorry! Failed to raise the request. Try Again.'
+              });
+              this.showModal();
+            }
+          );
       }
     });
   };
@@ -77,6 +105,7 @@ class CreateRequest extends React.Component {
   };
 
   render() {
+    const { visible, confirmLoading, ModalText } = this.state;
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: {
@@ -112,176 +141,186 @@ class CreateRequest extends React.Component {
     );
 
     return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <h2> Request Form</h2>
-        <Divider />
-        <Form.Item label="District" hasFeedback>
-          {getFieldDecorator('DistrictID', {
-            rules: [{ required: true, message: 'Please select district!' }],
-          })(
-            <Select placeholder="Please select a district" onChange={this.onDistrictChange}>
-              {this.props.districts.map(district =>
-                <Option key={district.DistrictID} 
-                  value={district.DistrictID}>
-                  {district.Name}
-                </Option>
-              )}
-            </Select>,
-          )}
-        </Form.Item>
-        <Form.Item label="Constituency" hasFeedback>
-          {getFieldDecorator('ConstituencyID', {
-            rules: [{ required: true, message: 'Please select constituency!' }],
-          })(
-            <Select placeholder="Please select a constituency">
-              {this.props.constituencies.map(constituency =>
-                <Option key={constituency.ConstituencyID} value={constituency.ConstituencyID}>{constituency.Name}</Option>
-              )}
-            </Select>,
-          )}
-        </Form.Item>
-        <Form.Item label="Mandal" hasFeedback>
-          {getFieldDecorator('MandalID', {
-            rules: [{ required: true, message: 'Please select mandal!' }],
-          })(
-            <Select placeholder="Please select a mandal" onChange={this.onMandalChange}>
-              {this.props.mandals.map(mandal =>
-                <Option key={mandal.MandalID} value={mandal.MandalID}>{mandal.Name}</Option>
-              )}
-            </Select>,
-          )}
-        </Form.Item>
-        <Form.Item label="Village" hasFeedback>
-          {getFieldDecorator('VillageID', {
-            rules: [{ required: true, message: 'Please select village!' }],
-          })(
-            <Select placeholder="Please select a village">
-              {this.props.villages.map(village =>
-                <Option key={village.VillageID} value={village.VillageID}>{village.Name}</Option>
-              )}
-            </Select>,
-          )}
-        </Form.Item>
-        <Form.Item
-          label={
-            <span>
-              First Name
-            </span>
-          }
-        >
-          {getFieldDecorator('FirstName', {
-            rules: [{ required: true, message: 'Please input your firstname!', whitespace: true }],
-          })(<Input />)}
-        </Form.Item>
-        <Form.Item
-          label={
-            <span>
-              Last Name
-            </span>
-          }
-        >
-          {getFieldDecorator('LastName', {
-            rules: [{ required: true, message: 'Please input your lastname!', whitespace: true }],
-          })(<Input />)}
-        </Form.Item>
-        <Form.Item label="Gender">
-          {getFieldDecorator('Gender')(
-            <Radio.Group>
-              <Radio value="Male">Male</Radio>
-              <Radio value="Female">Female</Radio>
-            </Radio.Group>,
-          )}
-        </Form.Item>
-        <Form.Item label="Age">
-                {getFieldDecorator('Age', {rules: [{ required: true, message: 'Please provide Age!' }] })(<InputNumber />)}
-                <span className="ant-form-text"> Years </span>
-        </Form.Item>
-        <Form.Item label="Phone Number">
-          {getFieldDecorator('MobileNumber', {
-            rules: [{ required: true, message: 'Please input your phone number!' }],
-          })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
-        </Form.Item>
-        <Form.Item label="E-mail">
-          {getFieldDecorator('EmailAddress', {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          })(<Input />)}
-        </Form.Item>
-        <Form.Item
-          label={
-            <span>
-              Address
-            </span>
-          }
-        >
-          {getFieldDecorator('Address', {
-            rules: [{ required: true, message: 'Please input your address!', whitespace: true }],
-          })(<TextArea rows={4} />)}
-        </Form.Item>
-        <Form.Item label="Benefits">
-          {getFieldDecorator('IssueCategory', {
-            rules: [{ required: true, message: 'Please input beneficiaries!' }],
-          })(
-            <Radio.Group>
-              <Radio value="Person">Person</Radio>
-              <Radio value="People">People</Radio>
-            </Radio.Group>,
-          )}
-        </Form.Item>
-        <Form.Item label="Estimated Budget">
-                {getFieldDecorator('Estimatedbudget', {rules: [{ required: true, message: 'Please provide Budget!' }] })(<InputNumber />)}
-                <span className="ant-form-text"> Rupees </span>
-        </Form.Item>
-        <Form.Item
-          label={
-            <span>
-              Brief Description
-            </span>
-          }
-        >
-          {getFieldDecorator('Description', {
-            rules: [{ required: true, message: 'Please input your description!', whitespace: true }],
-          })(<TextArea rows={4} />)}
-        </Form.Item>
-        <Form.Item label="Upload Relevant Docs">
-          <div className="dropbox">
-            {getFieldDecorator('Documents', {
-              valuePropName: 'fileList',
-              getValueFromEvent: this.normFile,
+      <React.Fragment>
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <h2> Request Form</h2>
+          <Divider />
+          <Form.Item label="District" hasFeedback>
+            {getFieldDecorator('DistrictID', {
+              rules: [{ required: true, message: 'Please select district!' }],
             })(
-              <Upload.Dragger name="files" action="http://api.magunta.in/api/Requests/PostFormData">
-                <p className="ant-upload-drag-icon">
-                  <Icon type="inbox" />
-                </p>
-                <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                <p className="ant-upload-hint">Supports single or bulk upload</p>
-              </Upload.Dragger>,
+              <Select placeholder="Please select a district" onChange={this.onDistrictChange}>
+                {this.props.districts.map(district =>
+                  <Option key={district.DistrictID}
+                    value={district.DistrictID}>
+                    {district.Name}
+                  </Option>
+                )}
+              </Select>,
             )}
-          </div>
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          {getFieldDecorator('Agreed', {
-            valuePropName: 'checked',
-          })(
-            <Checkbox>
-              I confirm all the above information provided is true upto my knowledge
+          </Form.Item>
+          <Form.Item label="Constituency" hasFeedback>
+            {getFieldDecorator('ConstituencyID', {
+              rules: [{ required: true, message: 'Please select constituency!' }],
+            })(
+              <Select placeholder="Please select a constituency">
+                {this.props.constituencies.map(constituency =>
+                  <Option key={constituency.ConstituencyID} value={constituency.ConstituencyID}>{constituency.Name}</Option>
+                )}
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item label="Mandal" hasFeedback>
+            {getFieldDecorator('MandalID', {
+              rules: [{ required: true, message: 'Please select mandal!' }],
+            })(
+              <Select placeholder="Please select a mandal" onChange={this.onMandalChange}>
+                {this.props.mandals.map(mandal =>
+                  <Option key={mandal.MandalID} value={mandal.MandalID}>{mandal.Name}</Option>
+                )}
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item label="Village" hasFeedback>
+            {getFieldDecorator('VillageID', {
+              rules: [{ required: true, message: 'Please select village!' }],
+            })(
+              <Select placeholder="Please select a village">
+                {this.props.villages.map(village =>
+                  <Option key={village.VillageID} value={village.VillageID}>{village.Name}</Option>
+                )}
+              </Select>,
+            )}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                First Name
+            </span>
+            }
+          >
+            {getFieldDecorator('FirstName', {
+              rules: [{ required: true, message: 'Please input your firstname!', whitespace: true }],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                Last Name
+            </span>
+            }
+          >
+            {getFieldDecorator('LastName', {
+              rules: [{ required: true, message: 'Please input your lastname!', whitespace: true }],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item label="Gender">
+            {getFieldDecorator('Gender')(
+              <Radio.Group>
+                <Radio value="Male">Male</Radio>
+                <Radio value="Female">Female</Radio>
+              </Radio.Group>,
+            )}
+          </Form.Item>
+          <Form.Item label="Age">
+            {getFieldDecorator('Age', { rules: [{ required: true, message: 'Please provide Age!' }] })(<InputNumber />)}
+            <span className="ant-form-text"> Years </span>
+          </Form.Item>
+          <Form.Item label="Phone Number">
+            {getFieldDecorator('MobileNumber', {
+              rules: [{ required: true, message: 'Please input your phone number!' }],
+            })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
+          </Form.Item>
+          <Form.Item label="E-mail">
+            {getFieldDecorator('EmailAddress', {
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                Address
+            </span>
+            }
+          >
+            {getFieldDecorator('Address', {
+              rules: [{ required: true, message: 'Please input your address!', whitespace: true }],
+            })(<TextArea rows={4} />)}
+          </Form.Item>
+          <Form.Item label="Benefits">
+            {getFieldDecorator('IssueCategory', {
+              rules: [{ required: true, message: 'Please input beneficiaries!' }],
+            })(
+              <Radio.Group>
+                <Radio value="Person">Person</Radio>
+                <Radio value="People">People</Radio>
+              </Radio.Group>,
+            )}
+          </Form.Item>
+          <Form.Item label="Estimated Budget">
+            {getFieldDecorator('Estimatedbudget', { rules: [{ required: true, message: 'Please provide Budget!' }] })(<InputNumber />)}
+            <span className="ant-form-text"> Rupees </span>
+          </Form.Item>
+          <Form.Item
+            label={
+              <span>
+                Brief Description
+            </span>
+            }
+          >
+            {getFieldDecorator('Description', {
+              rules: [{ required: true, message: 'Please input your description!', whitespace: true }],
+            })(<TextArea rows={4} />)}
+          </Form.Item>
+          <Form.Item label="Upload Relevant Docs">
+            <div className="dropbox">
+              {getFieldDecorator('Documents', {
+                valuePropName: 'fileList',
+                getValueFromEvent: this.normFile,
+              })(
+                <Upload.Dragger name="files" action="http://api.magunta.in/api/Requests/PostFormData">
+                  <p className="ant-upload-drag-icon">
+                    <Icon type="inbox" />
+                  </p>
+                  <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                  <p className="ant-upload-hint">Supports single or bulk upload</p>
+                </Upload.Dragger>,
+              )}
+            </div>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            {getFieldDecorator('Agreed', {
+              valuePropName: 'checked',
+            })(
+              <Checkbox>
+                I confirm all the above information provided is true upto my knowledge
             </Checkbox>,
-          )}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            SUBMIT
+            )}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              SUBMIT
           </Button>
-        </Form.Item>
-      </Form>
+          </Form.Item>
+        </Form>
+        <Modal
+          title="Request Status"
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>{ModalText}</p>
+        </Modal></React.Fragment>
+
     );
   }
 }

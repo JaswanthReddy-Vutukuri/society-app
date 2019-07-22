7e11306b7618ff959e5d6ed667f1fb6757a6975f
JaswanthReddy-Vutukuri <vutukurijaswanth515@gmail.com>
2019-07-22 12:47:57 +0530

    moved inline styles under cards component to external CSS

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,23 +6,23 @@ const RequestCounts = () => {
     <div>
     <Row gutter={16}>
       <Col span={6}>
-        <Card title="TOTAL REQUESTS" style={{background:'#0000ff5c',borderRadius:'10px', textAlign:'center', padding:'10px'}}>
-            <span style={{color:'white', fontSize:'2.5em'}}>1999<Icon type="swap" style={{marginLeft:'15px'}}/></span>
+        <Card title="TOTAL REQUESTS" className="req-card" style={{background:'#0000ff5c'}}>
+            <span className="req-value">1999<Icon type="swap" style={{marginLeft:'15px'}}/></span>
         </Card>
       </Col>
       <Col span={6}>
-        <Card title="APPROVED REQUESTS" style={{background:'#318a315e',borderRadius:'10px', textAlign:'center', padding:'10px'}}>
-        <span style={{color:'white', fontSize:'2.5em'}}>999<Icon type="check" style={{marginLeft:'15px'}}/></span>
+        <Card title="APPROVED REQUESTS" className="req-card" style={{background:'#318a315e'}}>
+        <span className="req-value">999<Icon type="check" style={{marginLeft:'15px'}}/></span>
         </Card>
       </Col>
       <Col span={6}>
-        <Card title="PENDING REQUESTS" style={{background:'#f3ab249e',borderRadius:'10px', textAlign:'center', padding:'10px'}}>
-        <span style={{color:'white', fontSize:'2.5em'}}>500<Icon type="warning" style={{marginLeft:'15px'}}/></span>
+        <Card title="PENDING REQUESTS" className="req-card" style={{background:'#f3ab249e'}}>
+        <span className="req-value">500<Icon type="warning" style={{marginLeft:'15px'}}/></span>
         </Card>
       </Col>
       <Col span={6}>
-        <Card title="DECLINED REQUESTS" style={{background:'#ff9999',borderRadius:'10px', textAlign:'center', padding:'10px'}}>
-        <span style={{color:'white', fontSize:'2.5em'}}>500<Icon type="dislike" style={{marginLeft:'15px'}}/></span>
+        <Card title="DECLINED REQUESTS" className="req-card" style={{background:'#ff9999'}}>
+        <span className="req-value">500<Icon type="dislike" style={{marginLeft:'15px'}}/></span>
         </Card>
       </Col>
     </Row>

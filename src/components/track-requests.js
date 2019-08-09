e470d0dade62dedba003a333dd741f8da39a30f0
JaswanthReddy-Vutukuri <vutukurijaswanth515@gmail.com>
2019-08-09 18:29:38 +0530

    added Track Ticket status logic

--- src/components/track-requests.js
+++ src/components/track-requests.js
@@ -6,6 +6,8 @@ import {
     Divider,
     Slider
 } from 'antd';
+import { requestService } from '../services';
+import ReqComments from './req-comments';
 
 const marks = {
     0: "Open",
@@ -17,11 +19,30 @@ const marks = {
 };
 
 class TrackRequestDetails extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedRequest: {},
+            ticket: ''
+        }
+    }
+
     handleSubmit = e => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                requestService.getRequests(values)
+                    .then(
+                        response => {
+                            console.log("response:",response,values.TicketNumber)
+                            this.setState({ selectedRequest: response.Results[0], ticket: values.TicketNumber });
+                        },
+                        error => {
+                            console.log("Error while fetching request:", error);
+                        }
+                    );
             }
         });
     };
@@ -34,6 +55,13 @@ class TrackRequestDetails extends React.Component {
         return e && e.fileList;
     };
 
+    handleOk = e => {
+        this.setState({
+            selectedRequest: {},
+            ticket: ''
+        })
+    }
+
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
@@ -41,7 +69,7 @@ class TrackRequestDetails extends React.Component {
                 <h2>Track Request</h2>
                 <Form layout="inline" onSubmit={this.handleSubmit} style={{ textAlign: 'center' }}>
                     <Form.Item label="Ticket Number" hasFeedback>
-                        {getFieldDecorator('ticket-number', {
+                        {getFieldDecorator('TicketNumber', {
                             rules: [{ required: true, message: 'Please input Ticket Number!' }],
                         })(
                             <Input placeholder="Input Ticket Number" />
@@ -53,7 +81,8 @@ class TrackRequestDetails extends React.Component {
                         </Button>
                     </Form.Item>
                     <Divider />
-                    <Slider marks={marks} defaultValue={40} style={{ width: '70%', margin: '15%' }} />
+                    {/* <Slider marks={marks} defaultValue={40} style={{ width: '70%', margin: '15%' }} /> */}
+                    {this.state.ticket ? <ReqComments request={this.state.selectedRequest} handleOk={this.handleOk} /> : null}
                     <Form.Item label="Estimated Completion">
                         <span className="ant-form-text">16.07.2019 Tuesday 12:00 PM</span>
                     </Form.Item>
